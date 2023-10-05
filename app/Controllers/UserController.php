680026ee9468e57bb2fed14312737adaad0b1c6c
Cely Febita <cely.febita21@students.unila.ac.id>
2023-10-05 20:13:18 +0700

    read-data

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -8,9 +8,24 @@ use App\Models\KelasModel;
 
 class UserController extends BaseController
 {
+    public $userModel;
+    public $kelasModel;
+
+    public function __construct(){
+        $this->userModel = new UserModel();
+        $this->kelasModel = new KelasModel();
+    }
+    
     public function index() 
     {
-        //
+    
+        $data = [
+            'title' => 'List User',
+            'users' => $this->userModel->getUser(),
+        ];
+
+        return view('list_user', $data);
+    
     }
     public function profile($nama='', $kelas='', $npm='')
     {
@@ -19,33 +34,13 @@ class UserController extends BaseController
             'kelas' => $kelas,
             'npm' => $npm
         ];
-        return view('profile', $data); #sampe sini jangan lupa jalankan php spark serve untuk akses ke browser
+        return view('profile', $data); //sampe sini jangan lupa jalankan php spark serve untuk akses ke browser
     }
 
     public function create(){
-        $kelas = [
-            [
-                'id' => 1,
-                'nama_kelas' => 'A'
-            ],
-
-            [
-                'id' => 2,
-                'nama_kelas' => 'B'
-            ],
-
-            [
-                'id' => 3,
-                'nama_kelas' => 'C'
-            ],
-
-            [
-                'id' => 4,
-                'nama_kelas' => 'D'
-            ],
-        ];
-
-
+        $kelasModel = new KelasModel();
+        $kelas = $this->kelasModel->getKelas();
+            
         if (session('validation') != null) {
             $validation = session('validation');
         } else {
@@ -54,53 +49,78 @@ class UserController extends BaseController
 
          $data = [
             'kelas' => $kelas,
-            'validation' => $validation,
+           'validation' => $validation,
+            'title' => 'Create User',
          ];
 
         return view('create_user', $data);
     }
 
     public function store(){
-        #dd($this->request->getVar());
-        #1
+        // dd($this->request->getVar());
+        // 1
 
-        $data = [
-            'nama' => $this->request->getVar('nama'),
-            'kelas' => $this->request->getVar('kelas'),
-            'npm' => $this->request->getVar('npm'),
-        ];
+        // $data = [
+        //     'nama' => $this->request->getVar('nama'),
+        //     'kelas' => $this->request->getVar('kelas'),
+        //     'npm' => $this->request->getVar('npm'),
+        // ];
 
-            if(!$this->validate([
-                'nama' => 'required',
-                'npm' => 'required|is_unique[user.npm]',
-           ])) {
-               $validation = \Config\Services::validation();
-               return redirect()->to('/user/create')->withInput()->with('validation', $validation);
-           }
+        if(!$this->validate([
+            'nama' => 'required',
+            'npm' => 'required|is_unique[user.npm]',
+       ])) {
+           $validation = \Config\Services::validation();
+           return redirect()->to('/user/create')->withInput()->with('validation', $validation);
+       }
 
-        $userModel = new UserModel();
-        $kelasModel = new KelasModel();
+       $this->userModel->saveUser([
+        'nama' => $this->request->getVar('nama'),
+        'npm' => $this->request->getVar('npm'),
+        'id_kelas' =>$this->request->getVar('kelas'),
+        ]);
+
+    return redirect()->to('/user');
 
 
-        $nama = $this->request->getPost('nama');
-        $npm = $this->request->getPost('npm');
-        $kelas = $this->request->getPost('kelas');
+        //dd($this->request->getVar());
+        //1
 
-        $data=[
-            'nama' => $nama,
-            'npm' => $npm,
-            'id_kelas' => $kelas
-        ];
+        // $data = [
+        //     'nama' => $this->request->getVar('nama'),
+        //     'kelas' => $this->request->getVar('kelas'),
+        //     'npm' => $this->request->getVar('npm'),
+        // ];
 
-        #2
-        $userModel->saveUser($data);
-        $data_new=[
-            'nama' => $nama,
-            'npm' => $npm,
-            'id_kelas' => $kelasModel->find($kelas)['nama_kelas']
-        ];
 
-        return view('profile', $data_new);
+        // $userModel = new UserModel();
+        // $kelasModel = new KelasModel();
+
+        // $nama = $this->request->getPost('nama');
+        // $npm = $this->request->getPost('npm');
+        // $kelas = $this->request->getPost('kelas');
+
+        // $data=[
+        //     'nama' => $nama,
+        //     'npm' => $npm,
+        //     'id_kelas' => $kelas,
+        // ];
+
+        // $data=[
+        //     'nama' => $nama,
+        //     'npm' => $npm,
+        //     'id_kelas' => $kelas
+        // ];
+
+        // return redirect()->to('/user/index');
+        // $this->$userModel->saveUser($data);
+        // $data_new=[
+        //     'nama' => $nama,
+        //     'npm' => $npm,
+        //     'id_kelas' => $kelasModel->find($kelas)['nama_kelas']
+        // ];
+
+        //return view('profile', $data_new);
 
         // $userModel->saveUser([
         //     'nama' => $this->request->getVar('nama'),
@@ -115,6 +135,7 @@ class UserController extends BaseController
         //     'npm' => $this->request->getVar('npm'),
         // ];
         // return view('profile', $data);
-        }
+
+    }
     
 }
--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?=base_url("assets/css/style.css")?>">
-    <title>Mahasiswa Ilmu Komputer</title>
-</head>
-<body>
-    <div class="container">
+<?= $this->extend('layouts/app') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container">
      <div class ="mhs">
         <form action="<?= base_url('/user/store') ?>" method="POST">
             <h1>Data Diri</h1>
@@ -55,6 +49,5 @@
     <img src="<?=base_url("assets/image/komputer.jpg")?>" alt="" />
     </div>
 </div>
-        
-</body>
-</html>
+
+<?= $this->endSection() ?>
